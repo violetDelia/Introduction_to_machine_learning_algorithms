f1891bfb96511c1705dbdd44e4a5d2a12ec27609
liufengrong <1733535832@qq.com>
2021-11-24 12:14:08 +0800

    感知机

--- 2.监督式学习算法基础/1.用感知机预测Iris属性.py
+++ 2.监督式学习算法基础/1.用感知机预测Iris属性.py
@@ -19,7 +19,7 @@ def load_data(remove_column):
 def show_scatter(data, x_column, y_column, color_column=None, size_column=None, sizes=None):
     sns.relplot(x=x_column, y=y_column, hue=color_column,
                 data=data, size=size_column, sizes=sizes)
-    
+
     plt.show()
 
 
@@ -34,22 +34,49 @@ def set_lable(data):
         lable_list.append(name)
     data["lable"] = data[data.columns[4]].replace(
         {lable_list[0]: -1, lable_list[1]: 1, lable_list[2]: 1})
-def show_results(data,x_column,y_column,color_column,w,b):
-    data["function_value"] = data[x_column]*w[0]+data[y_column]*w[1]+b
-    print(data[["function_value","lable"]])
-    
-    
+
+
+def show_results(data, x_column, y_column, color_column, w, b):
+    line_x = np.arange(data[x_column].agg(np.min), data[x_column].agg(np.max))
+    line_y = (-b-w[0]*line_x)/w[1]
+    sns.relplot(x=x_column, y=y_column, hue=color_column, data=data)
+    plt.plot(line_x, line_y)
+    plt.show()
+
+
+def get_accuracy(predict_y, test_y):
+    correct = 0
+    for i in range(predict_y.size):
+        if(predict_y[i] == test_y[i]):
+            correct += 1
+    return correct/predict_y.size
 
 
 if __name__ == "__main__":
     remove_column = ["Id"]
     data = load_data(remove_column)
     set_lable(data)
-    #show_scatter(data,data.columns[0],data.columns[1],data["lable"])
+    # show_scatter(data,data.columns[0],data.columns[1],data["lable"])
     train_x, test_x, train_y, test_y = get_train_and_test_data(
         data[[data.columns[0], data.columns[1]]], data["lable"], 0.3, 5)
     model = Perceptron()
-    print(model.__dict__)
     model.train(train_x.values, train_y.values)
-    print(model)
-    show_results(data,data.columns[0],data.columns[1],data["lable"],model.w,model.b)
+    print(model.learning_rate)
+    #记录信息
+    '''
+    train_x.to_csv("2.监督式学习算法基础\Iris_compute_info\\train_x.csv")
+    train_y.to_csv("2.监督式学习算法基础\Iris_compute_info\\train_y.csv")
+    test_x.to_csv("2.监督式学习算法基础\Iris_compute_info\\test_x.csv")
+    test_y.to_csv("2.监督式学习算法基础\Iris_compute_info\\test_y.csv")
+    show_results(
+        data, data.columns[0], data.columns[1], data["lable"], model.w, model.b)
+    predict_y = model.predict(test_x.values)
+    print(get_accuracy(predict_y, test_y.values))
+    for i in range(len(model.compute_info)):
+        filename = "2.监督式学习算法基础\Iris_compute_info\第{0}次迭代.csv".format(i)
+        model.compute_info[i].to_csv(filename
+        )
+    test_x["lable"] = test_y
+    test_x["predict"] = pd.DataFrame(pd.Series(predict_y.reshape(1, -1)[0],index=test_x.index))
+    test_x.to_csv("2.监督式学习算法基础\Iris_compute_info\预测结果.csv")
+    '''
--- 2.监督式学习算法基础/Perceptron.py
+++ 2.监督式学习算法基础/Perceptron.py
@@ -1,20 +1,37 @@
 import numpy as np
+import pandas as pd
 
 
 class Perceptron:
+    '''学习率'''
+    learning_rate = 1
+
+    '''设置学习率'''
+    def set_learning_rate(self, learning_rate):
+        self.learning_rate = learning_rate
+
     def train(self, X, Y):
         m, n = X.shape
-        print(X.shape)
+        '''初始w'''
         w = np.zeros((n, 1))
+        '''初始b'''
         b = 0
+        '''寻找到的标记'''
         is_find = False
         while not is_find:
             is_find = True
+            '''
+            遍历每个训练点，如果全都满足规则，跳出循环（可以设置为一定比例满足或者损失函数小于某个值）
+            '''
             for i in range(m):
                 x = X[i].reshape(1, -1)
-                if Y[i] * (x.dot(w)+b <= 0):
-                    w = w + Y[i] * x.T
-                    b = b+Y[i]
+                judge_value = Y[i] * (x.dot(w)+b)
+                if judge_value <= 0:
+                    '''
+                    迭代w，b
+                    '''
+                    w = w + Y[i] * x.T*self.learning_rate
+                    b = b+Y[i]*self.learning_rate
                     is_find = False
         self.w = w
         self.b = b
